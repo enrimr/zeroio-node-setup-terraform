dc89f18809eedbd77dabc9c0b48c219238a384ed
Enrique Mendoza <enriquemendozarobaina@gmail.com>
2021-05-21 10:29:50 +0200

    First version

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("keys/gcp-cryptocurrencies.json")
+
+  project = "cryptocurrencies-313407"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
