77997559f7b28def28d3b68377cd40d31e9f7491
Enrique Mendoza <enriquemendozarobaina@gmail.com>
2021-05-24 16:19:25 +0200

    Add variables file

--- main.tf
+++ main.tf
@@ -20,3 +20,27 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_address" "vm_static_ip" {
+  name = "zeroio-instance-0-static-ip"
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "zeroio-instance-0"
+  machine_type = "f1-micro"
+
+  tags = ["substrate", "zeroio", "node"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-minimal-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
